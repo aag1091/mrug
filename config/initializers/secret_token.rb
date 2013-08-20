932c8d727af1bd01801942f1be9b5bb6454bef0a
Avinash Gosavi <aag1091@gmail.com>
2013-08-20 09:06:54 +0530

    added foundation and few pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mrug::Application.config.secret_key_base = '70b863c33dd1ecc76527e45cc11e5a0331ca6d26009b26ee2da55ac3fe7707bdd55bc0ec8472b9d78aa0f7c623db8f150ec9e3d09acf383931ae39de235d423a'
